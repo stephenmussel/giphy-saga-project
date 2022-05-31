54f6446595cb4b6d6804a7d72d53da690dab082b
phaydara <phaydara@gmail.com>
2022-05-31 13:57:28 -0500

    displayed search results on DOM

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 function Search() {
 
     const [search, setSearch] = useState('');
+    const [results, setResults] = useState([]);
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -11,10 +12,20 @@ function Search() {
 
         console.log('this is input: ', search);
         setSearch('') // clears input
-    }
+
+        axios.post('/api/search', {search: search})
+            .then(response => {
+                console.log('response.data: ', response.data);
+                console.log('response.data...: ', response.data[0].images.original.url); //success, found giphy!
+                setResults(response.data)
+            }).catch(err => {
+                console.log('err in search: ', err);
+            });
+    };
 
     return(
         <div>
+            <div>
             <form onSubmit={handleSubmit}>
                 <input 
                     placeholder='search GIPHY'
@@ -27,6 +38,14 @@ function Search() {
                     value='search'
                 />
             </form>
+            </div>
+            <div>
+                <h1>Results...</h1>
+                {/* {JSON.stringify(results)} */}
+                {results.map((gif, i) => (
+                    <img key={i} src={gif.images.original.url}/>
+                ))}
+            </div>
         </div>
     )
 }
