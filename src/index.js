bf3c3e65ad7d62eb10305831f798807316166931
phaydara <phaydara@gmail.com>
2022-05-31 10:24:04 -0500

    set up redux store and sagas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { Provider } from 'react-redux';
+import { createStore, combineReduxers, applyMiddleware, combineReducers } from 'redux';
+import { logger } from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
+
+function* rootSaga() {
+
+}
+
+// category reducer
+const category = (state =[], action) => {
+    switch (action.type) {
+    
+        default:
+            return state;
+    }
+}
+
+// favorite reducer
+const favorite = (state =[], action) => {
+    switch (action.type) {
+    
+        default:
+            return state;
+    }
+}
+
+const store = createStore(
+    combineReducers({
+        category,
+        favorite
+    }),
+    applyMiddleware(logger, sagaMiddleware),
+);
+
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
