af7cc23409dc40de70fb45723da56518fd0798e5
phaydara <phaydara@gmail.com>
2022-05-31 13:12:16 -0500

    moved Search into component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Search from '../Search/Search';
 
 function App(props) {
   return (
     <div>
       <h1>Giphy Search!</h1>
+      <Search />
     </div>
   );
 }
