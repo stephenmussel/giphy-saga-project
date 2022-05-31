4145657c0f3fb4981e6f3b6da146ad7860ee47e4
phaydara <phaydara@gmail.com>
2022-05-31 17:16:19 -0500

    added fetchFavorites on page load

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,4 +1,19 @@
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
 function Favorites() {
+
+    const favorites = useSelector(store => store.favorite)
+
+    const fetchFavorites = () => {
+        console.log('in fetchFavorites!');
+    }
+
+    useEffect(() => {
+        console.log('in useEffect');
+        fetchFavorites();
+    }, [])
+
     return (
         <>
             <h3>Favorites...</h3>
