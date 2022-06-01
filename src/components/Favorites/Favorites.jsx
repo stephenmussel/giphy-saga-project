6af8cffe4076cd535c071b1b3ec7b3cd07b1bd6a
phaydara <phaydara@gmail.com>
2022-06-01 14:36:45 -0500

    added sortBy options and captured event of option selected

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import FavoritesItem from '../FavoritesItem/FavoritesItem';
 
 function Favorites() {
@@ -7,10 +7,12 @@ function Favorites() {
     const favorites = useSelector(store => store.favorite)
     const dispatch = useDispatch();
 
+    const [sortCat, setSortCat] = useState('n/a')
+
     const fetchFavorites = () => {
         console.log('in fetchFavorites!');
 
-        dispatch({type: 'FETCH_FAVORITES'});
+        dispatch({ type: 'FETCH_FAVORITES' });
     };
 
     useEffect(() => {
@@ -18,17 +20,33 @@ function Favorites() {
         fetchFavorites();
     }, [])
 
+    const sortBy = (event) => {
+        console.log('in sortBy!');
+        setSortCat(event.target.value);
+        console.log('catId:', sortCat);
+    }
+
     return (
-        <>
+        <div>
             <h1>Favorites...</h1>
+            <div>
+                <select onChange={sortBy}>
+                    <option>Sort by:</option>
+                    <option value="1">Funny</option>
+                    <option value="2">Cohort</option>
+                    <option value="3">Cartoon</option>
+                    <option value="4">NSFW</option>
+                    <option value="5">Meme</option>
+                </select>
+            </div>
             {/* {JSON.stringify(favorites)} */}
             {favorites.map(each => (
-                <FavoritesItem 
+                <FavoritesItem
                     each={each}
                     key={each.id}
                 />
             ))}
-        </>
+        </div>
     )
 }
 
