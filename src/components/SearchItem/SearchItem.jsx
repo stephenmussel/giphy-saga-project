e0711e6c88b8ce1c7da12443fe85fe169d5a5bcc
phaydara <phaydara@gmail.com>
2022-06-15 20:20:06 -0500

    created toggle button for favorite and unfavorite

--- src/components/SearchItem/SearchItem.jsx
+++ src/components/SearchItem/SearchItem.jsx
@@ -1,11 +1,14 @@
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 function SearchItem({ gif }) {
 
     const dispatch = useDispatch();
+    const [button, setButton] = useState(true);
 
     const createFavorite = () => {
         console.log('clicked favorite!');
+        setButton(!button);
 
         // sends favorite gif (url) to createFavorite saga
         const action = { type: 'CREATE_FAVORITE', payload: gif };
@@ -19,8 +22,9 @@ function SearchItem({ gif }) {
                 alt="gif"
                 style={{ marginTop: 25, marginBottom: 5 }}
             /><br />
-
-            <button onClick={createFavorite}>favorite</button>
+            {button ? <button onClick={createFavorite}>favorite</button> : <button onClick={createFavorite}>unfavorite</button>}
+            {/* <button onClick={createFavorite}>favorite</button> */}
+            {/* <button onClick={createFavorite}>favorite</button> */}
         </div>
     )
 }
